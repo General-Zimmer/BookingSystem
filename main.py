e217d0ad303aa49fcf47eb81de5eb8b8281c7781
GZimmer <kristianz1000@gmail.com>
2020-11-16 09:54:07 +0100

    Volle did something

--- main.py
+++ main.py
@@ -7,43 +7,41 @@ isstring = la.is_string_with_space
 sql = database.database()
 
 print(datetime.datetime.now())
+def data():
+    navnset = False
+    while navnset == False:
+        print("Skriv navn")
+        navn = str(input())
+        print(isstring(navn))
+        if isstring(navn) == True:
+            res = res(navn)
+            navnset = True
 
-navnset = False
-while navnset == False:
-    print("Skriv navn")
-    navn = str(input())
-    print(isstring(navn))
-    if isstring(navn) == True:
-        res = res(navn)
-        navnset = True
+    datoset = False
+    while datoset == False:
+        print("Enter a date in YYYY-MM-DD format")
+        dato = input()
+        try:
+            year, month, day, = res.dato(dato)
+            stryear = str(year)
+            strmonth = str(month)
+            strday = str(day)
+            datoset = res.datetest(month, day, strmonth, strday, stryear)
+        except:
+            pass
 
-datoset = False
-while datoset == False:
-    print("Enter a date in YYYY-MM-DD format")
-    dato = input()
-    try:
-        year, month, day, = res.dato(dato)
-        stryear = str(year)
-        strmonth = str(month)
-        strday = str(day)
-        datoset = res.datetest(month, day, strmonth, strday, stryear)
-    except:
-        pass
+    timeset = False
+    while timeset == False:
+        print("Enter a time in HH:MM format")
+        time = input()
+        try:
+            hour, minute = res.time(time)
+            strmin = str(minute)
+            strhour = str(hour)
+            timeset = res.timetest(hour, minute, strhour, strmin)
+        except:
+            pass
+    finaldate = str(res.final(year, month, day, hour, minute))
 
-timeset = False
-while timeset == False:
-    print("Enter a time in HH:MM format")
-    time = input()
-    try:
-        hour, minute = res.time(time)
-        strmin = str(minute)
-        strhour = str(hour)
-        timeset = res.timetest(hour, minute, strhour, strmin)
-    except:
-        pass
 
-finaldate = str(res.final(year, month, day, hour, minute))
 
-sql.add(str(navn), str(finaldate))
-
-print("Reservation for", navn, "til", finaldate)
--- tests.py
+++ tests.py
@@ -1,3 +1,26 @@
 import datetime
+import lagic as la
+import database
 
-print(mydb)
+res = la.reservation
+isstring = la.is_string_with_space
+sql = database.database()
+
+print("Hvad vil du? "
+      "1: Tilføj"
+      "2: Ændrer"
+      "3: Slet")
+valg = int(input())
+
+if valg == 1:
+    data()
+    sql.add(str(navn), str(finaldate))
+    print("Reservation for", navn, "til", finaldate)
+elif valg == 2:
+    print("Hvem skal ændres?")
+    valgnavn = input()
+    sql.modify(valgnavn)
+elif valg == 3:
+    print("Hvem skal slettes?")
+    valgnavn = input()
+    sql.delete(valgnavn)
